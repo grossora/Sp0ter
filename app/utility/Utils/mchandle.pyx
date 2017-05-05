41f409b1f03f6d01c761f9cda9f40450833adde3
grossora <grossora.neutrino@gmail.com>
2017-05-05 14:34:36 -0500

    pushing in quck... needs to be cleaned up

--- app/utility/Utils/mchandle.pyx
+++ app/utility/Utils/mchandle.pyx
@@ -12,6 +12,224 @@ zhi = geo.GetZ_Bounds()[1]
 
 
 
+
+def mc_neutron_induced_OBJ_2( f ):
+    tf = ROOT.TFile("{}".format(f))
+    tree = tf.Get("mcShower")
+   # tree = tf.Get("mcShower")
+
+    _x_particle = []
+    _y_particle = []
+    _z_particle = []
+    _Ex_particle = []
+    _Ey_particle = []
+    _Ez_particle = []
+    _pp_particle = []
+    pi0_4vect = []
+    pi0_4mom = []
+    daughter_4pair = []
+
+    pi0_mothersid=[]
+    pi0_partid=[]
+
+    for i in tree:
+        print 'we are in the tree'
+        motherpdg_list = [x for x in i.mcshower_motherPdg]
+        motherid_list = [x for x in i.mcshower_motherId]
+        motherprocess_list = [x for x in i.mcshower_motherProcess]
+        #id_list = [x for x in i.mcparticle_]
+        _x_particle = [x for x in i.mcshower_endX]
+        _y_particle = [x for x in i.mcshower_endY]
+        _z_particle = [x for x in i.mcshower_endZ]
+        _t_particle = [x for x in i.mcshower_endT]
+        _px_particle = [x for x in i.mcshower_startPX]
+        _py_particle = [x for x in i.mcshower_startPY]
+        _pz_particle = [x for x in i.mcshower_startPZ]
+        _e_particle = [x for x in i.mcshower_startE]
+
+        _x_motherparticle = [x for x in i.mcshower_motherEndX]
+        _y_motherparticle = [x for x in i.mcshower_motherEndY]
+        _z_motherparticle = [x for x in i.mcshower_motherEndZ]
+        _t_motherparticle = [x for x in i.mcshower_motherEndT]
+        _px_motherparticle = [x for x in i.mcshower_motherEndPX]
+        _py_motherparticle = [x for x in i.mcshower_motherEndPY]
+        _pz_motherparticle = [x for x in i.mcshower_motherEndPZ]
+        _e_motherparticle = [x for x in i.mcshower_motherEndE]
+
+        pi0_mothersid=[]
+        pi0_partid=[]
+        for i in range(len(motherpdg_list)):
+            if motherpdg_list[i]==111 and motherprocess_list[i]=='neutronInelastic' :
+                if motherid_list[i] in pi0_mothersid:
+                    tg_4vect = []
+                    tg_4mom = []
+                    tg_4vect.append(_x_particle[i])
+                    tg_4vect.append(_y_particle[i])
+                    tg_4vect.append(_z_particle[i])
+                    tg_4vect.append(_t_particle[i])
+                    tg_4mom.append(_px_particle[i])
+                    tg_4mom.append(_py_particle[i])
+                    tg_4mom.append(_pz_particle[i])
+                    tg_4mom.append(_e_particle[i])
+                    daughter_4pair.append([tg_4vect,tg_4mom])
+                    continue
+
+                if _x_motherparticle[i]<xlo or  _x_motherparticle[i]>xhi or  _y_motherparticle[i]<ylo or  _y_motherparticle[i]>yhi or  _z_motherparticle[i]<zlo or  _z_motherparticle[i]>zhi:
+                    continue
+
+                pi0_mothersid.append(motherid_list[i])
+                pi0_4vect.append(_x_motherparticle[i])
+                pi0_4vect.append(_y_motherparticle[i])
+                pi0_4vect.append(_z_motherparticle[i])
+                pi0_4vect.append(_t_motherparticle[i])
+                pi0_4mom.append(_px_motherparticle[i])
+                pi0_4mom.append(_py_motherparticle[i])
+                pi0_4mom.append(_pz_motherparticle[i])
+                pi0_4mom.append(_e_motherparticle[i])
+
+                tg_4vect = []
+                tg_4mom = []
+                tg_4vect.append(_x_particle[i])
+                tg_4vect.append(_y_particle[i])
+                tg_4vect.append(_z_particle[i])
+                tg_4vect.append(_t_particle[i])
+                tg_4mom.append(_px_particle[i])
+                tg_4mom.append(_py_particle[i])
+                tg_4mom.append(_pz_particle[i])
+                tg_4mom.append(_e_particle[i])
+                daughter_4pair.append([tg_4vect,tg_4mom])
+
+
+
+        if len(pi0_mothersid)!=1:
+            print ' We dont signal !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!j'
+            pi0_4vect = []
+            pi0_4mom = []
+            [pi0_4vect.append(-99999) for x in range(4)]
+            [pi0_4mom.append(-99999) for x in range(4)]
+            daughter_4pair = []
+            td = [-99999 for x in range(4)]
+            daughter_4pair.append([td,td])
+            return False ,pi0_4vect,pi0_4mom, daughter_4pair
+
+    return True , pi0_4vect, pi0_4mom, daughter_4pair
+
+
+
+def mc_neutron_induced_contained_2( f ):
+    tf = ROOT.TFile("{}".format(f))
+    tree = tf.Get("mcShower")
+
+    _x_particle = []
+    _y_particle = []
+    _z_particle = []
+    _Ex_particle = []
+    _Ey_particle = []
+    _Ez_particle = []
+    _pp_particle = []
+    pi0_4vect = []
+    pi0_4mom = []
+    daughter_4pair = []
+
+    pi0_mothersid=[]
+    pi0_partid=[]
+    contained = True
+
+    for i in tree:
+        print 'we are in the tree'
+        motherpdg_list = [x for x in i.mcshower_motherPdg]
+        motherid_list = [x for x in i.mcshower_motherId]
+        motherprocess_list = [x for x in i.mcshower_motherProcess]
+        #id_list = [x for x in i.mcparticle_]
+        _x_particle = [x for x in i.mcshower_endX]
+        _y_particle = [x for x in i.mcshower_endY]
+        _z_particle = [x for x in i.mcshower_endZ]
+        _t_particle = [x for x in i.mcshower_endT]
+        _px_particle = [x for x in i.mcshower_startPX]
+        _py_particle = [x for x in i.mcshower_startPY]
+        _pz_particle = [x for x in i.mcshower_startPZ]
+        _e_particle = [x for x in i.mcshower_startE]
+
+        _x_motherparticle = [x for x in i.mcshower_motherEndX]
+        _y_motherparticle = [x for x in i.mcshower_motherEndY]
+        _z_motherparticle = [x for x in i.mcshower_motherEndZ]
+        _t_motherparticle = [x for x in i.mcshower_motherEndT]
+        _px_motherparticle = [x for x in i.mcshower_motherEndPX]
+        _py_motherparticle = [x for x in i.mcshower_motherEndPY]
+        _pz_motherparticle = [x for x in i.mcshower_motherEndPZ]
+        _e_motherparticle = [x for x in i.mcshower_motherEndE]
+
+        pi0_mothersid=[]
+        pi0_partid=[]
+        for i in range(len(motherpdg_list)):
+            if motherpdg_list[i]==111 and motherprocess_list[i]=='neutronInelastic' :
+                if motherid_list[i] in pi0_mothersid:
+                    if _x_particle[i]<xlo or  _x_particle[i]>xhi or  _y_particle[i]<ylo or  _y_particle[i]>yhi or  _z_particle[i]<zlo or  _z_particle[i]>zhi:
+                        contained = False
+                        # Give up
+                        break
+                    tg_4vect = []
+                    tg_4mom = []
+                    tg_4vect.append(_x_particle[i])
+                    tg_4vect.append(_y_particle[i])
+                    tg_4vect.append(_z_particle[i])
+                    tg_4vect.append(_t_particle[i])
+                    tg_4mom.append(_px_particle[i])
+                    tg_4mom.append(_py_particle[i])
+                    tg_4mom.append(_pz_particle[i])
+                    tg_4mom.append(_e_particle[i])
+                    daughter_4pair.append([tg_4vect,tg_4mom])
+                    continue
+
+                if _x_motherparticle[i]<xlo or  _x_motherparticle[i]>xhi or  _y_motherparticle[i]<ylo or  _y_motherparticle[i]>yhi or  _z_motherparticle[i]<zlo or  _z_motherparticle[i]>zhi:
+                    continue
+
+                pi0_mothersid.append(motherid_list[i])
+                pi0_4vect.append(_x_motherparticle[i])
+                pi0_4vect.append(_y_motherparticle[i])
+                pi0_4vect.append(_z_motherparticle[i])
+                pi0_4vect.append(_t_motherparticle[i])
+                pi0_4mom.append(_px_motherparticle[i])
+                pi0_4mom.append(_py_motherparticle[i])
+                pi0_4mom.append(_pz_motherparticle[i])
+                pi0_4mom.append(_e_motherparticle[i])
+
+                if _x_particle[i]<xlo or  _x_particle[i]>xhi or  _y_particle[i]<ylo or  _y_particle[i]>yhi or  _z_particle[i]<zlo or  _z_particle[i]>zhi:
+                    contained = False
+                    # Give up
+                    break
+                tg_4vect = []
+                tg_4mom = []
+                tg_4vect.append(_x_particle[i])
+                tg_4vect.append(_y_particle[i])
+                tg_4vect.append(_z_particle[i])
+                tg_4vect.append(_t_particle[i])
+                tg_4mom.append(_px_particle[i])
+                tg_4mom.append(_py_particle[i])
+                tg_4mom.append(_pz_particle[i])
+                tg_4mom.append(_e_particle[i])
+                daughter_4pair.append([tg_4vect,tg_4mom])
+
+
+
+        if len(pi0_mothersid)!=1 or not contained:
+            print ' We dont signal !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!j'
+            pi0_4vect = []
+            pi0_4mom = []
+            [pi0_4vect.append(-99999) for x in range(4)]
+            [pi0_4mom.append(-99999) for x in range(4)]
+            daughter_4pair = []
+            td = [-99999 for x in range(4)]
+            daughter_4pair.append([td,td])
+            return False ,pi0_4vect,pi0_4mom, daughter_4pair
+
+    return True , pi0_4vect, pi0_4mom, daughter_4pair
+
+
+
+
+
+
 ############################## 
 ## Def for no pi0 back evt  ## 
 ############################## 
@@ -308,6 +526,83 @@ def mc_neutron_induced_OBJ( f ):
     return True ,pi0_4mom, daughter_4mom
 
 
+def mcpart_tshift_2(pobj_list):
+    toff = 400 
+    #
+
+    xshift = pobj_list[1][0] + (pobj_list[1][3]/1000 )*(0.11)
+    #xshift = pobj_list[1][0] + (pobj_list[1][3]/1000 )*(0.11)
+    #xshift = pobj_list[1][0] + (pobj_list[1][3]/1000 +2381.8)*(0.11)*0.5
+    #xshift = pobj_list[1][0] + (pobj_list[1][3]/1000 -3200+800+400)*(0.11)*0.5
+    #xshift = pobj_list[1][0] + (pobj_list[1][3]/1000 )*(0.11)
+    #xshift = pobj_list[1][0] + (pobj_list[1][3]/1000 + toff)*(0.11)
+    #xshift = pobj_list[1][0] + (pobj_list[1][3]/1000 + toff)*(0.1114)
+    #xshift = pobj_list[1][0] + (pobj_list[1][3]+toff)*(0.1114)
+    #xshift = pobj_list[1][0] + (pobj_list[1][3]+toff)*(11./ 100000)
+    #xshift = pobj_list[1][0] + pobj_list[1][3]*(11./ 100000)
+    print 'about the shift'
+    print pobj_list
+    print 'This is x ' , str(pobj_list[1][0])
+    print ' this is the offset with drift ' ,str((pobj_list[1][3]/1000)*(0.11))
+    #print ' this is the offset with drift ' ,str((pobj_list[1][3]/1000)*(0.11))
+    #print ' this is the offset with drift ' ,str((pobj_list[1][3]/1000 -3200+400+800)*(0.11)*0.5)
+    #print ' this is the offset with drift ' ,str((pobj_list[1][3]/1000)*(0.11))
+    pobj_list[1][0] = xshift
+    for i in range(len(pobj_list[3])):
+        #for d in range(len(pobj_list[3][i])):
+        dxshift = pobj_list[3][i][0][0] + (pobj_list[3][i][0][3]/1000 )*(0.11)
+        #dxshift = pobj_list[3][i][0][0] + (pobj_list[3][i][0][3]/1000 )*(0.11)
+        #dxshift = pobj_list[3][i][0][0] + (pobj_list[3][i][0][3]/1000  -3200+400+800)*(0.11)*0.5
+        #dxshift = pobj_list[3][i][0][0] + (pobj_list[3][i][0][3]/1000+toff)*(0.1114)
+        pobj_list[3][i][0][0] = dxshift
+
+    return pobj_list
+
+def mc_Obj_points_2(pobj_list):
+    #print obj_list
+    #incoming pobj_list looks like this  [Bool, pi_4vect, pi0_4mom , [ [daug4vect ,daugt4mom] ]
+    obj_list = pobj_list[1:]
+    pi0_xyz = obj_list[0]
+    gamma_xyz = [x[0] for x in obj_list[2]]
+
+    # The first entry in the list is a 4 position of the pi0
+    space = 20*20*20 +  20* 3*len(gamma_xyz)
+    dataset = [None for x in range(space)]
+    #dataset = []
+    mclab = 0 # This is hard code magic for coloring in the bee viewer
+    box_size = 1
+    density = 5
+    # lower number is higher density
+    # Fix this later on# lower number is higher density
+
+
+    counter=0
+    for i in range(0,box_size*100,density):
+        for j in range(0,box_size*100,density):
+            for k in range(0,box_size*100,density):
+                dataset[counter] = [pi0_xyz[0]-box_size +2.*box_size*k/100.,pi0_xyz[1]-box_size+2.*box_size*j/100.,pi0_xyz[2]-box_size+2.*box_size*i/100.]
+                counter+=1
+		#dataset.append([pi0_xyz[0]-box_size +2.*box_size*k/100.,pi0_xyz[1]-box_size+2.*box_size*j/100.,pi0_xyz[2]-box_size+2.*box_size*i/100.])
+
+    # Draw a cross for the photons
+    for a in range(len(gamma_xyz)):
+        for i in range(0,box_size*100,density):
+            dataset[counter] = [gamma_xyz[a][0]-box_size +2.*box_size*i/100.,gamma_xyz[a][1],gamma_xyz[a][2]]# wali the X
+            counter+=1
+            dataset[counter] = [gamma_xyz[a][0],gamma_xyz[a][1]-box_size +2.*box_size*i/100.,gamma_xyz[a][2]]# wali the X
+            counter+=1
+            dataset[counter] = [gamma_xyz[a][0],gamma_xyz[a][1],gamma_xyz[a][2]-box_size +2.*box_size*i/100.]# wali the X
+            counter+=1
+
+    # Draw a line between the pi0vtx and daughters
+
+    # We are going to fill out a box around the region
+    labels = [0 for a in range(len(dataset))]
+    return dataset , labels
+ 
+
+
+
 def mc_Obj_points(pobj_list):
     #print obj_list
     obj_list = pobj_list[1:]
@@ -349,6 +644,160 @@ def mc_Obj_points(pobj_list):
     labels = [0 for a in range(len(dataset))]
     return dataset , labels
  
+
+def piz_mc_info_2(infile):
+    tf = ROOT.TFile("{}".format(infile))
+    tree = tf.Get("mcShower")
+
+    _x_particle = []
+    _y_particle = []
+    _z_particle = []
+    _Ex_particle = []
+    _Ey_particle = []
+    _Ez_particle = []
+    _pp_particle = []
+
+    pi0_4vect = []
+    pi0_4mom = []
+    daughter_4pair = []
+
+    pi0_mothersid=[]
+    pi0_partid=[]
+    contained = True
+
+    for i in tree:
+        print 'we are in the tree'
+        motherpdg_list = [x for x in i.mcshower_motherPdg]
+        motherid_list = [x for x in i.mcshower_motherId]
+        motherprocess_list = [x for x in i.mcshower_motherProcess]
+        #id_list = [x for x in i.mcparticle_]
+        _x_particle = [x for x in i.mcshower_endX]
+        _y_particle = [x for x in i.mcshower_endY]
+        _z_particle = [x for x in i.mcshower_endZ]
+        _t_particle = [x for x in i.mcshower_endT]
+        _px_particle = [x for x in i.mcshower_startPX]
+        _py_particle = [x for x in i.mcshower_startPY]
+        _pz_particle = [x for x in i.mcshower_startPZ]
+        _e_particle = [x for x in i.mcshower_startE]
+
+        _x_motherparticle = [x for x in i.mcshower_motherEndX]
+        _y_motherparticle = [x for x in i.mcshower_motherEndY]
+        _z_motherparticle = [x for x in i.mcshower_motherEndZ]
+        _t_motherparticle = [x for x in i.mcshower_motherEndT]
+        _px_motherparticle = [x for x in i.mcshower_motherEndPX]
+        _py_motherparticle = [x for x in i.mcshower_motherEndPY]
+        _pz_motherparticle = [x for x in i.mcshower_motherEndPZ]
+        _e_motherparticle = [x for x in i.mcshower_motherEndE]
+
+        pi0_mothersid=[]
+        pi0_partid=[]
+
+
+        p_gamma_x = -1.
+        p_gamma_y = -1.
+        p_gamma_z = -1
+        p_gamma_2_x = -1
+        p_gamma_2_y = -1
+        p_gamma_2_z = -1
+        p_gamma_mag = -1
+        p_gamma_2_mag = -1
+
+
+
+
+
+
+        for i in range(len(motherpdg_list)):
+            if motherpdg_list[i]==111 and motherprocess_list[i]=='neutronInelastic' :
+                if motherid_list[i] in pi0_mothersid:
+                    if _x_particle[i]<xlo or  _x_particle[i]>xhi or  _y_particle[i]<ylo or  _y_particle[i]>yhi or  _z_particle[i]<zlo or  _z_particle[i]>zhi:
+                        contained = False
+                        # Give up
+                        break
+                    tg_4vect = []
+                    tg_4mom = []
+                    tg_4vect.append(_x_particle[i])
+                    tg_4vect.append(_y_particle[i])
+                    tg_4vect.append(_z_particle[i])
+                    tg_4vect.append(_t_particle[i])
+                    tg_4mom.append(_px_particle[i])
+                    tg_4mom.append(_py_particle[i])
+                    tg_4mom.append(_pz_particle[i])
+                    tg_4mom.append(_e_particle[i])
+                    daughter_4pair.append([tg_4vect,tg_4mom])
+                    continue
+
+                if _x_motherparticle[i]<xlo or  _x_motherparticle[i]>xhi or  _y_motherparticle[i]<ylo or  _y_motherparticle[i]>yhi or  _z_motherparticle[i]<zlo or  _z_motherparticle[i]>zhi:
+                    continue
+
+                pi0_mothersid.append(motherid_list[i])
+                pi0_4vect.append(_x_motherparticle[i])
+                pi0_4vect.append(_y_motherparticle[i])
+                pi0_4vect.append(_z_motherparticle[i])
+                pi0_4vect.append(_t_motherparticle[i])
+                pi0_4mom.append(_px_motherparticle[i])
+                pi0_4mom.append(_py_motherparticle[i])
+                pi0_4mom.append(_pz_motherparticle[i])
+                pi0_4mom.append(_e_motherparticle[i])
+
+                if _x_particle[i]<xlo or  _x_particle[i]>xhi or  _y_particle[i]<ylo or  _y_particle[i]>yhi or  _z_particle[i]<zlo or  _z_particle[i]>zhi:
+                    contained = False
+                    # Give up
+                    break
+                tg_4vect = []
+                tg_4mom = []
+                tg_4vect.append(_x_particle[i])
+                tg_4vect.append(_y_particle[i])
+                tg_4vect.append(_z_particle[i])
+                tg_4vect.append(_t_particle[i])
+                tg_4mom.append(_px_particle[i])
+                tg_4mom.append(_py_particle[i])
+                tg_4mom.append(_pz_particle[i])
+                tg_4mom.append(_e_particle[i])
+                daughter_4pair.append([tg_4vect,tg_4mom])
+
+
+        #
+        vtx_pi_x = pi0_4vect[0] # Convert to OFFSET
+        vtx_pi_y = pi0_4vect[1]
+        vtx_pi_z = pi0_4vect[2]
+        p_pi_x = pi0_4mom[0]/pi0_4mom[3]
+        p_pi_y = pi0_4mom[1]/pi0_4mom[3]
+        p_pi_z = pi0_4mom[2]/pi0_4mom[3]
+        p_pi_mag = pi0_4mom[3]
+        # Check if it's dalitz
+        if len(daughter_4pair)!=2:
+            # Bail
+            fill = str('-1 ')*24
+            bails = fill.rsplit(' ',1)[0]
+            return bails
+
+        vtx_gamma_x = daughter_4pair[0][0][0] # Need to OFFSET
+        vtx_gamma_y = daughter_4pair[0][0][1]
+        vtx_gamma_z = daughter_4pair[0][0][2]
+        p_gamma_x = daughter_4pair[0][1][0]/daughter_4pair[0][1][3]
+        p_gamma_y = daughter_4pair[0][1][1]/daughter_4pair[0][1][3]
+        p_gamma_z = daughter_4pair[0][1][2]/daughter_4pair[0][1][3]
+        p_gamma_mag = daughter_4pair[0][1][3]
+
+        vtx_gamma_2_x = daughter_4pair[1][0][0] # Need to OFFSET
+        vtx_gamma_2_y = daughter_4pair[1][0][1]
+        vtx_gamma_2_z = daughter_4pair[1][0][2]
+        p_gamma_2_x = daughter_4pair[1][1][0]/daughter_4pair[1][1][3]
+        p_gamma_2_y = daughter_4pair[1][1][1]/daughter_4pair[1][1][3]
+        p_gamma_2_z = daughter_4pair[1][1][2]/daughter_4pair[1][1][3]
+        p_gamma_2_mag = daughter_4pair[1][1][3]
+        
+        gamma_angle = math.acos((p_gamma_x*p_gamma_2_x+p_gamma_y*p_gamma_2_y+p_gamma_z*p_gamma_2_z))
+        # Form the return string
+        dalitz = 0
+        ret = str(dalitz)+' '+str(vtx_pi_x)+' '+str(vtx_pi_y)+' '+str(vtx_pi_z)+' '+str(p_pi_x)+' '+str(p_pi_y)+' '+str(p_pi_z)+' '+str(p_pi_mag)+' '+str(vtx_gamma_x)+' '+str(vtx_gamma_y)+' '+str(vtx_gamma_z)+' '+str(p_gamma_x)+' '+str(p_gamma_y)+' '+str(p_gamma_z)+' '+str(p_gamma_mag)+' '+str(vtx_gamma_2_x)+' '+str(vtx_gamma_2_y)+' '+str(vtx_gamma_2_z)+' '+str(p_gamma_2_x)+' '+str(p_gamma_2_y)+' '+str(p_gamma_2_z)+' '+str(p_gamma_2_mag)+' '+str(gamma_angle) + ' '+ str(1-math.cos(gamma_angle))
+        return ret
+
+
+
+
+
 
 
 def piz_mc_info(infile):
@@ -588,6 +1037,11 @@ def gamma_mc_dep(infile):
         qdep +=en.q
     ret = str(qdep)
     return ret
+
+
+
+
+
 
 
 
